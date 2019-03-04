e0bd4fbef09d2f353025f2932742711554b77102
Xyzhao <xyzhao@xyzhao.com>
2019-03-04 17:34:07 +0800

    增加 font awesome

--- src/main.js
+++ src/main.js
@@ -8,8 +8,21 @@ import iview from 'iview'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
 
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+
 import 'iview/dist/styles/iview.css'
 Vue.config.productionTip = false
+
+// font awesome
+library.add(fas, far, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
