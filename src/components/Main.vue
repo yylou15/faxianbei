7a0be2b7c3caaf567e7f2095707f8c1eff887b52
louyuanyang <louyuanyang@outlook.com>
2019-03-10 12:47:21 +0800

    细节修改

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,8 +19,8 @@ export default {
     return {
       bgList: [
           {url:"/static/img/main/1.jpg",title:'人生最好的旅行，就是你在一个陌生的地方，发现一种久违的感动'},
-          {url:"/static/img/main/3.jpg",title:'一颗说走就走的心，一个会拍照的情侣，一段甜蜜的旅程'},
           {url:"/static/img/main/2.jpg",title:'有一种旅行，是在旅行的路上感知自我'},
+          {url:"/static/img/main/3.jpg",title:'一颗说走就走的心，一个会拍照的情侣，一段甜蜜的旅程'},
           {url:"/static/img/main/4.jpg",title:'携三五好友去旅行吧，无论在哪，只要身边是他们就好'}
       ]
     }
